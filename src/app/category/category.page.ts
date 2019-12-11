72c6e4ec5d3e65e7675463655f13ca9bf92ae28f
adavid <212673582@ge.com>
2019-12-11 14:15:35 +0100

    beginning of category page

--- src/app/category/category.page.ts
+++ src/app/category/category.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { AuthService } from '../shared/auth.service';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpBackend, HttpHeaders } from '@angular/common/http';
+import { Storage } from '@ionic/storage';
+
 
 @Component({
   selector: 'app-category',
@@ -8,12 +12,30 @@ import { AuthService } from '../shared/auth.service';
 })
 export class CategoryPage implements OnInit {
 
+  private API_URL = environment.API_URL_DEV;
+  private categories;
+
   constructor(
     @Inject(AuthService)
-    private authService: AuthService
-  ) { }
-
-  ngOnInit() {
+    private authService: AuthService,
+    handler: HttpBackend, 
+    private http: HttpClient,
+    public storage: Storage) {
+    this.http = new HttpClient(handler);
   }
 
+  ngOnInit() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Bearer ' + this.authService.getToken()
+      })
+    };
+
+    let categories = this.http.get(`${this.API_URL}/category`, httpOptions); //need to add userId
+    categories.subscribe(
+      result => {
+        this.categories = result;
+      });
+  }
 }
