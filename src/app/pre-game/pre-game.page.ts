713ba3381abfb38fbdd4b3e05915a6d119a46fe8
Kevin Limacher <kevin.limacher@gmail.com>
2019-12-17 13:56:25 +0100

    create and delete session

--- src/app/pre-game/pre-game.page.ts
+++ src/app/pre-game/pre-game.page.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { AuthService } from '../shared/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient, HttpErrorResponse, HttpBackend, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-pre-game',
@@ -6,10 +12,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pre-game.page.scss'],
 })
 export class PreGamePage implements OnInit {
-
-  constructor() { }
+  private API_URL = environment.API_URL_DEV;
+  private sessionId;
+  constructor(
+    @Inject(AuthService)
+    private authService: AuthService,
+    private toastr: ToastrService,
+    private handler: HttpBackend, 
+    private http: HttpClient,
+    private route: Router) {
+    this.http = new HttpClient(handler);
+  }
 
   ngOnInit() {
+    //todo check if user has a current session
+    //else redirect to home
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Bearer ' + this.authService.getToken()
+      })
+    };
+    const userid =  this.authService.getLoggedUser().userid;
+    let user = this.http.get<any>(`${this.API_URL}/user/${userid}`, httpOptions);
+    user.subscribe(
+      result => {
+        this.sessionId = result.session.sessionId;
+        console.log('session :'+this.sessionId);
+
+      });
+  }
+
+  quit(){
+    this.http.delete<any>(`${this.API_URL}/session/delete/${this.sessionId}`).
+    subscribe(
+      result => {},
+      error => {
+        this.toastr.error(error, 'Quit session error');
+      },
+      () => {
+        this.toastr.success('Session successfully quitted', 'Session');
+        this.route.navigateByUrl("/host");
+      }
+    );
   }
 
 }
